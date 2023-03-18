696b9d23dc7b7c67623f19a99128a032b129d7b4
Anjali <anjalipandey01052001@gmail.com>
2023-03-18 20:04:17 +0530

    leetcode kth missing problem added

---
+++ Binary_Search/kth_missing_pos.java
@@ -0,0 +1,21 @@
+class kth_missing_pos {
+    public static void main(String[] args) {
+        int arr[] = {2,3,4,7,11};
+        int k = 5;
+        int ans=findKthPositive(arr,k);
+        System.out.println(ans);
+    }
+        static int findKthPositive(int[] arr, int k) {
+            int left = 0, right = arr.length - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (arr[mid] - mid - 1 < k) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            return left + k;
+        }
+    }
+
