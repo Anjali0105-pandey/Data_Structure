dc3d62aeddf44e50be1705b2ec35b76930b6cbaf
Anjali <anjalipandey01052001@gmail.com>
2023-05-25 13:46:32 +0530

    factorial added

--- Recursion I/swap_nodes_in_pairs.java
+++
@@ -1,5 +0,0 @@
-/*Following the guidelines we lay out above, we can implement the function as follows:
-
-First, we swap the first two nodes in the list, i.e. head and head.next;
-Then, we call the function self as swap(head.next.next) to swap the rest of the list following the first two nodes.
-Finally, we attach the returned head of the sub-list in step (2) with the two nodes swapped in step (1) to form a new linked list. */
